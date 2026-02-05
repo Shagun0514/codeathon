f30ba8b93710ffbed718bb1c2366e8e7e60976bf
Webwizard242 <kashishrawat318@gmail.com>
2026-02-05 11:21:41 +0530

    Add quiz submission route with authentication

---
+++ Business Logic/src/routes/quiz.js
@@ -0,0 +1,5 @@
+const router = require("express").Router();
+const quizController = require("../controller/quizController");
+const auth = require("../middleware/auth");
+router.post("/submit/:quizId", auth, quizController.submitQuiz);
+module.exports = router;
